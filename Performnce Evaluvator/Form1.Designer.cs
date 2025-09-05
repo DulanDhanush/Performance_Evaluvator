5909a29fb1d0c33e16afcd0ef926c5857106f490
Dulan Dhanush <dulandhanush0@gmail.com>
2025-09-05 23:21:02 +0530

    Add files via upload

---
+++ Performnce Evaluvator/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Performnce_Evaluvator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.EIDtxt = new System.Windows.Forms.TextBox();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ptxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Teamtxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Inovationtxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Cbutton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.AvgTxt = new System.Windows.Forms.TextBox();
+            this.GradeTXT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // EIDtxt
+            // 
+            this.EIDtxt.Location = new System.Drawing.Point(212, 27);
+            this.EIDtxt.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.EIDtxt.Name = "EIDtxt";
+            this.EIDtxt.Size = new System.Drawing.Size(148, 26);
+            this.EIDtxt.TabIndex = 1;
+            this.EIDtxt.TextChanged += new System.EventHandler(this.EIDtxt_TextChanged);
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.Location = new System.Drawing.Point(212, 63);
+            this.Nametxt.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(148, 26);
+            this.Nametxt.TabIndex = 3;
+            this.Nametxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Ptxt
+            // 
+            this.Ptxt.Location = new System.Drawing.Point(287, 223);
+            this.Ptxt.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Ptxt.Name = "Ptxt";
+            this.Ptxt.Size = new System.Drawing.Size(148, 26);
+            this.Ptxt.TabIndex = 5;
+            this.Ptxt.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(158, 227);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Productivity";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Teamtxt
+            // 
+            this.Teamtxt.Location = new System.Drawing.Point(287, 259);
+            this.Teamtxt.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Teamtxt.Name = "Teamtxt";
+            this.Teamtxt.Size = new System.Drawing.Size(148, 26);
+            this.Teamtxt.TabIndex = 7;
+            this.Teamtxt.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(156, 263);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Team Work";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Inovationtxt
+            // 
+            this.Inovationtxt.Location = new System.Drawing.Point(287, 295);
+            this.Inovationtxt.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Inovationtxt.Name = "Inovationtxt";
+            this.Inovationtxt.Size = new System.Drawing.Size(148, 26);
+            this.Inovationtxt.TabIndex = 9;
+            this.Inovationtxt.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(156, 299);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Inovation";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Cbutton
+            // 
+            this.Cbutton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Cbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cbutton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Cbutton.Location = new System.Drawing.Point(179, 372);
+            this.Cbutton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Cbutton.Name = "Cbutton";
+            this.Cbutton.Size = new System.Drawing.Size(233, 45);
+            this.Cbutton.TabIndex = 10;
+            this.Cbutton.Text = "Calculate";
+            this.Cbutton.UseVisualStyleBackColor = false;
+            this.Cbutton.Click += new System.EventHandler(this.Cbutton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Firebrick;
+            this.CloseButton.Location = new System.Drawing.Point(836, 496);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(113, 35);
+            this.CloseButton.TabIndex = 11;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // AvgTxt
+            // 
+            this.AvgTxt.Location = new System.Drawing.Point(108, 475);
+            this.AvgTxt.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.AvgTxt.Name = "AvgTxt";
+            this.AvgTxt.Size = new System.Drawing.Size(148, 26);
+            this.AvgTxt.TabIndex = 12;
+            // 
+            // GradeTXT
+            // 
+            this.GradeTXT.Location = new System.Drawing.Point(400, 475);
+            this.GradeTXT.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.GradeTXT.Name = "GradeTXT";
+            this.GradeTXT.Size = new System.Drawing.Size(148, 26);
+            this.GradeTXT.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 480);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Average";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(303, 478);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Grade";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.LightGray;
+            this.label8.Location = new System.Drawing.Point(268, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(388, 29);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Employee Productivity Evaluator";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Performnce_Evaluvator.Properties.Resources.work_5382501_640;
+            this.panel1.Location = new System.Drawing.Point(568, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 388);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Location = new System.Drawing.Point(-7, -7);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(984, 70);
+            this.panel2.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.EIDtxt);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Nametxt);
+            this.groupBox1.Location = new System.Drawing.Point(37, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(482, 101);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Employee Details";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.Location = new System.Drawing.Point(642, 496);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 35);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(981, 545);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.GradeTXT);
+            this.Controls.Add(this.AvgTxt);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.Cbutton);
+            this.Controls.Add(this.Inovationtxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Teamtxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Ptxt);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Employee Performance Evaluvator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EIDtxt;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Ptxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Teamtxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Inovationtxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Cbutton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.TextBox AvgTxt;
+        private System.Windows.Forms.TextBox GradeTXT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Performnce Evaluvator/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Performnce_Evaluvator
+{
+    public partial class Form1 : Form
+    {
+        public double avg = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EIDtxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Calculation Button
+        private void Cbutton_Click(object sender, EventArgs e)
+        {
+            if (EIDtxt.Text == "" || EIDtxt.Text == "")
+            {
+                MessageBox.Show("Please enter the details!");
+                return;
+            }else
+            {
+                if (Convert.ToDouble(Ptxt.Text) > 0 || Convert.ToString(Ptxt.Text) == ""||Convert.ToDouble( Ptxt.Text) < 100 ||
+                    Convert.ToString(Teamtxt.Text)== ""||
+                    Convert.ToDouble(Teamtxt.Text)>0 || Convert.ToDouble(Teamtxt.Text)>100 ||
+                    Convert.ToString(Teamtxt.Text)== ""||
+                    Convert.ToDouble(Inovationtxt.Text)>0 || Convert.ToDouble(Inovationtxt.Text)>100)
+                {
+                    avg = (Convert.ToDouble(Teamtxt.Text)+Convert.ToDouble(Ptxt.Text)+Convert.ToDouble(Inovationtxt.Text)) / 3.00;
+                    if (avg >= 80)
+                    {
+                        this.BackColor = Color.Aqua;
+                        this.GradeTXT.Text = "Excelent";
+                        this.AvgTxt.Text = $"{avg}";
+                    }
+                    else if (avg > 60)
+                    {
+                        this.BackColor = Color.Yellow;
+                        this.AvgTxt.Text = $"{avg}";
+                        this.GradeTXT.Text = "Good";
+                    
+                    }
+                    else if (avg > 40)
+                    {
+                        this.BackColor = Color.Pink;
+                        this.GradeTXT.Text = "Satisfactory";
+                        this.AvgTxt.Text = $"{avg}";
+                        
+                    }
+                    else
+                    {
+                        this.BackColor = Color.Red;
+                        this.GradeTXT.Text = $"Poor";
+                        this.AvgTxt.Text = $"{avg}";
+
+                    }
+                }else
+                {
+                    MessageBox.Show("Invalied inputs!");
+                    this.Teamtxt.Text = "";
+                    this.Ptxt.Text = "";
+                    this.Inovationtxt.Text = "";
+                    
+                }
+            }
+           
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.EIDtxt.Text = "";
+            this.Nametxt.Text = "";
+            this.Ptxt.Text = "";
+            this.Inovationtxt.Text = "";
+            this.Teamtxt.Text = "";
+            this.BackColor = Color.White;
+            this.GradeTXT.Text = "";
+            this.AvgTxt.Text = "";
+            MessageBox.Show("Cleared!");
+        }
+    }
+}
